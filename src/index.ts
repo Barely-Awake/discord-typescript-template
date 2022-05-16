c52057292f0b6abf0b8ea2db10264e6f45fae6c4
Barely-Awake <ryanasperger@gmail.com>
2022-05-16 09:13:10 -0700

    Made clientCollections variable instead of having commands added onto client. Made commandAdder function be able to go into folders in commands folder Did the same for eventHandler function

--- src/index.ts
+++ src/index.ts
@@ -5,48 +5,62 @@ import config from './utils/readConfig.js';
 const intents = new Discord.Intents();
 intents.add(Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES);
 
+const client = new Discord.Client({intents: intents});
+const clientCollections = {
+  commands: new Discord.Collection(),
+  events: new Discord.Collection(),
+};
 
-const client: any = new Discord.Client({ intents: intents, });
+export { clientCollections as default };
 
-export { client as default };
-
-client.commands = new Discord.Collection();
 commandAdder();
-
-client.events = new Discord.Collection();
 eventHandler();
 
-async function commandAdder() {
-  const commandFiles = await readdir('./dist/commands');
+async function commandAdder(pathAdditions: string = '') {
+  const commandFiles = await readdir('./dist/commands' + pathAdditions);
 
   for (const file of commandFiles) {
 
-    if (!file.endsWith('.js') || file.startsWith('_'))
+    if (file.startsWith('_') || (file.includes('.') && !file.endsWith('.js')))
       continue;
 
-    const command = await import(`./commands/${file}`);
+    if (!file.endsWith('.js')) {
+      await commandAdder(pathAdditions + '/' + file);
+      continue;
+    }
+
+    const command = await import(`./commands${pathAdditions}/${file}`);
     let commandName = file.split('.')[0];
 
-    client.commands.set(commandName, command.default.execute);
+    clientCollections.commands.set(commandName, command.default);
+    console.log(`Loaded command: ${commandName}`);
+    if (command.description.aliases === undefined)
+      continue;
+
+    for (let i in command.description.aliases) {
+      clientCollections.commands.set(command.description.aliases[i], command.default);
+    }
   }
 }
 
-async function eventHandler() {
-  const eventFiles = await readdir('./dist/events');
+async function eventHandler(pathAdditions: string = '') {
+  const eventFiles = await readdir('./dist/events' + pathAdditions);
 
   for (const file of eventFiles) {
 
-    if (!file.endsWith('.js') || file.startsWith('_'))
+    if (file.startsWith('_') || (file.includes('.') && !file.endsWith('.js')))
       continue;
 
+    if (!file.endsWith('.js'))
+      await eventHandler(pathAdditions + '/' + file);
 
-    const event = await import(`./events/${file}`);
+    const event = await import(`./events${pathAdditions}/${file}`);
 
     let eventName = file.split('.')[0];
-    if (event.default.settings.once)
-      client.once(eventName, (...args: string[]) => event.default.execute(...args));
+    if (event.settings.once)
+      client.once(eventName, (...args: string[]) => event.default(...args));
     else
-      client.on(eventName, (...args: string[]) => event.default.execute(...args));
+      client.on(eventName, (...args: string[]) => event.default(...args));
   }
 }
 
