8647b2bf56a971401b08a91ce2e5e6312a4ee7e6
Barely-Awake <ryanasperger@gmail.com>
2022-05-16 09:13:59 -0700

    Added botName to config types

--- src/utils/readConfig.ts
+++ src/utils/readConfig.ts
@@ -1,14 +1,14 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
 export function readConfig(): Config {
-  const configFile = readFileSync("config.json");
+  const configFile = readFileSync('config.json');
   return JSON.parse(configFile.toString());
 }
-
 
 interface Config {
   token: string;
   prefix: string;
+  botName: string;
 }
 
 const config = readConfig();
