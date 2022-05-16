cc29cbf6a6628e5ae9296b5463186f8537e5b19d
Barely-Awake <ryanasperger@gmail.com>
2022-05-16 09:13:40 -0700

    Reformatted messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,32 +1,29 @@
-import client from "../index.js";
-import * as Discord from "discord.js";
-import config from "../utils/readConfig.js";
+import clientCollections from '../index.js';
+import * as Discord from 'discord.js';
+import config from '../utils/readConfig.js';
 
-async function execute(message: Discord.Message) {
+export default async function (message: Discord.Message) {
   commandHandler(message);
 }
-
-export default {
-  settings: {
-    once: false,
-  },
-  execute: execute,
-};
 
 function commandHandler(message: Discord.Message) {
   if (!message.content.startsWith(config.prefix))
     return;
 
-  const messageArray = message.content.split(" ");
+  const messageArray = message.content.split(' ');
 
   let commandName = messageArray[0].toLowerCase();
   commandName = commandName.slice(config.prefix.length);
   const args = messageArray.slice(1);
 
-  const command = client.commands.get(commandName);
+  const command = clientCollections.commands.get(commandName);
 
-  if (!command)
+  if (!command || typeof command !== 'function')
     return;
 
   command(message, args);
 }
+
+export const settings = {
+  once: false,
+};
