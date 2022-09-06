61fe0a1572b0cdf3ef827970f6f70a3a7dc2f65b
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2022-09-05 18:07:10 -0700

    change(bot): Moved exampleCategory to comments

--- src/bot/helpCategories.ts
+++ src/bot/helpCategories.ts
@@ -3,13 +3,18 @@ import { EmbedBuilder, SelectMenuComponentOptionData } from 'discord.js';
 // Keep this updated with the keys of the category info object
 export type CommandCategory = 'info' | 'exampleCategory';
 
+/*
+* Object Keys should be filled out following the template below
+* <pre>
+* exampleCategory: {
+*   description: 'This field will show up under the description of the category',
+*     label: 'This will be the user facing name of the category',
+*     value: 'exampleCategory', // Value should be the same as the object key
+*     embed: new EmbedBuilder(),
+* }
+* </pre>
+*/
 export const categoryInfo: { [index: string]: CategoryInfo } = {
-  exampleCategory: {
-    description: 'This field will show up under the description of the category',
-    label: 'This will be the user facing name of the category',
-    value: 'exampleCategory', // Value should be the same as the object key
-    embed: new EmbedBuilder(),
-  },
   info: {
     description: 'Commands providing basic information like the help command',
     label: 'Info',
